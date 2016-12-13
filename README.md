0a46edb08a4b97ceee65042aadf39a7883f4e2f3
kKapalka <k.kapalka@hotmail.com>
2016-12-13 14:31:22 +0100

    Modyfikacja

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # szyfr-cardano
 Szyfr Cardano. Język - C/C++
 
+Version 0.2
+-	Zastosowano klasy i pogrupowano kod w bardziej przejrzyste funkcje
+
+
 Schemat działania:
 - 	W terminalu przyjmuje 1 argument: plik z tekstem do zaszyfrowania/odszyfrowania
-- 	Wybieramy, czy chcemy zaszyfrować, czy odszyfrować (lub podajemy to jako argument opcjonalny)
+- 	Wybieramy, czy chcemy zaszyfrować, czy odszyfrować
 -	W wypadku szyfrowania: program generuje klucz i zapisuje go w pliku "klucz.txt"
 -	Szyfr Cardano podmienia litery alfabetu w tekście szyfrowanym na litery w kluczu wg zasady: 'a'-1.litera klucza, 'b'-druga litera klucza itp.
 -	Rozszyfrowanie: program ładuje klucz zapisany w pliku "klucz.txt" i porównuje każdą literę tekstu z kluczem
--- szyfr.cpp
+++ szyfr.cpp
@@ -3,122 +3,49 @@
 #include <fstream>
 #include <stdlib.h>
 #include <time.h>
-//A is ASCII 65, Z is 90
-//a is ascii 97, z is 122
-// spacebar is 32
+//**           Wyrzuca Warningi z powodu:
+//** deprecated conversion fron string const to char*
+//**               mozna zignorowac
 using namespace std;
+//*********************FUNKCJE GLOBALNE********************************//
+string cryptoPrep(string src){
+        for (size_t i = 0;i<src.length();i++) if (src[i]<97 ||src[i]>122) src=src.erase(i--,1);
+	return src;
+} //preparuje tresc tak, aby zawieral tylko male litery alfabetu
 
-string szyfr(string text, string mode, string cipher);
-string crypto_prep(string src);
-string generate();
-bool alphacheck(string a);
-
-int main(int argc, char *argv[]){
-srand(time(NULL));
-if (argc<2) cout<<"Nieprawidlowa ilosc argumentow"<<endl;
-else{
-	string tryb;
-	fstream klucz;
-	fstream tekst;
-	
-	tekst.open(argv[1]);
-	if (tekst.is_open()){
-		string tresc ((istreambuf_iterator<char>(tekst)), istreambuf_iterator<char>());
-		tresc = crypto_prep(tresc);
-//wczytywanie tekstu
-		if (argc==2){
-			cout<<"Czy chcesz zaszyfrowac czy rozszyfrowac tekst? 'encr'/'decr'"<<endl;
-			cin>>tryb;
-		}
-//opcja recznego wybierania trybu dzialania 'encr'/'decr'
-		else tryb=argv[2];
-		tekst.close();
-		tekst.open(argv[1], std::fstream::out | std::fstream::trunc);
-		if(tryb[0]=='e' || tryb[0]=='s' || tryb[0]=='z'){
-			//pierwsze litery slow "encrypt" || "szyfruj" || "zaszyfruj"
-			string cardano;
-			cardano = generate();			
-			tresc = szyfr(tresc,"encr",cardano);
- 			cout<<"Tekst zostal zaszyfrowany. Oto efekt: "<<endl<<tresc<<endl;
-			tekst<<tresc;	
-			klucz.open("klucz.txt", std::fstream::out | std::fstream::trunc);
-			klucz<<cardano;
-			cout<<"Wygenerowany klucz: "<<endl<<cardano<<endl;
-			tekst.close();
-			klucz.close();
-			}
-		else if(tryb[0]=='d' ||tryb[0]=='o' || tryb[0]=='r'){
-			//pierwsze litery slow "decrypt" || "odszyfruj" || "rozszyfruj"
-			klucz.open("klucz.txt");
-			if(!klucz.is_open()){
-				cout<<"Nie znaleziono klucza."<<endl;
-				tekst<<tresc;
-				tekst.close();
-				return 1;
-			}
-			string cardano ((istreambuf_iterator<char>(klucz)), istreambuf_iterator<char>());
-			cardano=crypto_prep(cardano);
-			if(!alphacheck(cardano) || cardano.length()!=26){
-				cout<<"Plik 'klucz.txt' nie jest kluczem"<<endl;
-				tekst<<tresc;
-				tekst.close();
-				return 1;
-			}
-			tresc = szyfr(tresc, "decr",cardano);
- 			cout<<"Tekst zostal rozszyfrowany. Oto efekt: "<<endl<<tresc<<endl;			
-			tekst<<tresc;
-			cout<<"Zastosowany klucz: "<<endl<<cardano<<endl;
-			klucz.close();
-			tekst.close();
-			}
-		else cout<<"Nie rozpoznano trybu."<<endl;
-		}
-		
-else cout<<"Nie powiodlo sie wczytywanie tekstu"<<endl;
-}
-return 0;
-}
-
-string szyfr(string text, string mode, string cipher){
-	if (mode=="encr")for(size_t i=0;i<text.length()-1;i++) text[i]=cipher[text[i]-97];
-	else if(mode=="decr"){
-		for(size_t i=0;i<text.length()-1;i++){
-			for (int j=0;j<26;j++){
-				if (text[i]==cipher[j]){
-					text[i]=j+97;
-					break;
-				}
-			}
-		}
-	}
-	return text;
-}
-
-string crypto_prep(string src)
-{
-    if (src.length() == 0) {
-        return src;
-    }
-
-    for (size_t idx = 0;idx<src.length();idx++){
-	if (src[idx]<97 ||src[idx]>122) src=src.erase(idx--,1);
-
-    }
-
-    return src;
-}
-//**************FUNKCJA GENERUJACA KLUCZ *******************//
 string generate(){
-string key;
-int singular;
-string alphanum="abcdefghijklmnopqrstuvwxyz";
-for(int i=0;i<26;i++){
-	singular=rand()%alphanum.length();
-	key+=alphanum[singular];
-	alphanum.erase(singular,1);
-}
-return key;
-}
+	string key;
+	int singular;
+	string alphanum="abcdefghijklmnopqrstuvwxyz";
+	for(int i=0;i<26;i++){
+		singular=rand()%alphanum.length();
+		key+=alphanum[singular];
+		alphanum.erase(singular,1);
+	}
+	return key;
+}//funkcja generujaca klucz
+
+
+string load(char *filename){
+	fstream file;
+	file.open(filename);
+	if(!file.is_open()){
+		cout<<"Nie powiodlo sie ladowanie pliku "<<filename<<endl;
+		return "";
+	}
+	string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+	content=cryptoPrep(content);
+	file.close();
+	return content;
+} //funkcja ladujaca zawartosc pliku 'filename' do zmiennej. Zwraca "" w razie niepowodzenia
+
+void overwrite(char *filename, string text){
+	fstream file;
+	file.open(filename, std::fstream::out | std::fstream::trunc);
+	file<<text;
+	file.close();
+} //Tworzy/Nadpisuje plik o nazwie 'filename' i zapelnia go trescia 'text'
+
 bool alphacheck(string a){
 	int tab[26]={0};
 	for(int i=0;i<26;i++){
@@ -126,4 +53,62 @@ bool alphacheck(string a){
 		if(tab[a[i]-97]==2)return false;
 	}
 	return true;	
-}	
+} //test klucza - czy zawiera 26 roznych malych liter alfabetu
+
+//*******************KONIEC FUNKCJE GLOBALNE*************************//
+//**********************Klasa Cardano *******************************//
+class Cardano{
+public:
+
+//----------------- KONSTRUKTORY I FUNKCJE -------------------------//
+Cardano(string _text=""):text(_text){}
+
+void encrypt(string &text, string &key){
+	for(size_t i=0;i<text.length()-1;i++) text[i]=key[text[i]-97];
+} //szyfrowanie
+
+void decrypt(string &text, string &key){
+	for(size_t i=0;i<text.length()-1;i++){
+		for (int j=0;j<26;j++){
+			if (text[i]==key[j]){
+				text[i]=j+97;
+				break;
+			}
+		}
+	}
+} //rozszyfrowanie
+
+void Run(){
+	if (text=="") return;
+	if(mode[0]=='e' || mode[0]=='s' || mode[0]=='z'){
+		key=generate();
+		encrypt(text,key);
+		overwrite("klucz.txt",key);
+		}
+	else if(mode[0]=='d' ||mode[0]=='o' || mode[0]=='r'){
+		key=load("klucz.txt");
+		if(key=="" || key.length()!=26 || !alphacheck(key))return;
+		decrypt(text,key);
+		overwrite("klucz.txt","");
+		}
+} //glowna funkcja szyfrujaca - obsluga trybow, modyfikacja plikow, zapis (przy szyfrowaniu) i usuwanie (przy rozszyfrowaniu) klucza
+
+string text; //tresc pliku
+string key; //klucz
+string mode; //tryb
+};
+
+
+
+int main(int argc, char *argv[]){
+if(argc>2)return 1;
+srand(time(NULL));
+Cardano A(load(argv[1]));
+cout<<"Ustaw tryb dzialania (encr/decr): ";
+cin>>A.mode;
+
+A.Run();
+	cout<<A.text<<endl;
+	overwrite(argv[1],A.text);
+return 0;
+};
